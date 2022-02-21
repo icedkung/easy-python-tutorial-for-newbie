2236a33e5161c1e905126ca875cf9be1f8425215
Arnon Sangngern <77655933+icedkung@users.noreply.github.com>
2022-02-21 18:46:47 +0700

    :dizzy: [Practice #5] Basic While Loop

---
+++ whileloopBasic.py
@@ -0,0 +1,12 @@
+ 
+print("ระบุหมายเลข")
+while True:
+    x=int(input("หมายเลข :"))
+    if x == 1:
+      print('นก')
+    elif x==2:
+      print('นก')
+      
+    elif x == "stop":
+       break
+

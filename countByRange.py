652c98f7ad0f75c910a604e3fa31f6e07a26f8d7
Arnon Sangngern <77655933+icedkung@users.noreply.github.com>
2022-02-21 18:34:53 +0700

    Create countByRange.py

---
+++ countByRange.py
@@ -0,0 +1,20 @@
+import datetime
+def fnumcount():
+  nowsart = datetime.datetime.now()
+  print ("เวลาเริ่มคือ : ")
+  print (nowsart.strftime("%Y-%m-%d %H:%M:%S\n\n"))
+
+  #ชื่อโปรแกรม
+  print('โปรแกรมนับเลข')
+
+  #รับค่า 3 ค่า
+  fnum = int(input('1st num = '))
+  rnum = int(input('range num = '))
+  lnum = int(input('last num = '))
+
+  print('\n')
+  print('ผลลัพธ์')
+  print(list(range(fnum,lnum,rnum)))
+
+
+fnumcount()

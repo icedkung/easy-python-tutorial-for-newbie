5278b7bbaec4ac47aeeca9cacb69443d4fce7418
Arnon Sangngern <77655933+icedkung@users.noreply.github.com>
2022-02-21 18:43:57 +0700

    :dizzy: [Pracitice#3] Sorting

---
+++ sort.py
@@ -0,0 +1,25 @@
+
+def sortnum():
+ #define 
+ number=[]
+
+ #loop input
+ print("โปรแกรมเรียงตัวเลข ระบุเลขที่ต้องการเรียง และระบุ 0 เพื่อเริ่มเรียง")
+ while True:
+     x=int(input("Num (0=หยุด) :"))
+     if x<=0:
+         break
+     number.append(x)
+
+#ickdev sorting tutorial
+
+ #sorted output
+ print("\nก่อนเรียง=>",number)
+ number.sort()
+ print("\nน้อยไปมาก=>",number)
+ number.reverse()
+ print("\nมากไปน้อย=>",number)
+ print("เรียบร้อยแล้ว !")
+
+#function call
+sortnum()

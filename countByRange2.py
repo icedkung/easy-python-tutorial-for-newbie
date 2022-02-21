169bec1ea5f56ce150e0951769832386e1b4f3cc
Arnon Sangngern <77655933+icedkung@users.noreply.github.com>
2022-02-21 18:40:00 +0700

    :dizzy: [Pracitice#2]  Count Number By Range 2

---
+++ countByRange2.py
@@ -0,0 +1,21 @@
+def qnumcount():
+ #ส่งไฟล์ได้ที่ https://iamickdev.com/workshop/studen_submit/thepyzero
+ #ส่งไฟล์ได้ที่ https://iamickdev.com/workshop/studen_submit/thepyzero
+ #ส่งไฟล์ได้ที่ https://iamickdev.com/workshop/studen_submit/thepyzero
+ #Intro to program
+ name = str(input('Your name : '))
+ print('Hello',name)
+ print(name,'นี่คือระบบนับตัวเลข')
+ print('นี่คือระบบนับเลขแบบกำหนดระยะ แบบทดสอบ 2')
+ #แจ้งเตือนก่อนรับค่า
+ ii = input('กดปุ่มใดก็ได้เพื่อเริ่ม! ')
+ #รับค่า
+ fnum = int(input('first Number = '))
+ rnum = int(input('range Number = '))
+ lnum = int(input('last Number = '))
+ print(list(range(fnum,lnum,rnum)))
+''' jlr team teaching for everyone '''
+#function call
+qnumcount()
+
+
